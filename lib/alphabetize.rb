7b9b1b0a314e978bca8ae3cc70baeafb9753a14e
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-18 16:11:02 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,43 @@
 def alphabetize(arr)
-  # code here
+  result_array = []
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet_array = esperanto_alphabet.split("")
+
+   alphabet_array.each do |letter|
+
+     arr.each do |phrase|
+      if phrase[0] == letter
+        result_array << phrase
+      end
+    end
+  end
+  result_array
 end
+final_array = []
+  result_array.each_with_index do |word, index|
+
+       if index > 0
+        i = 0
+        while i < word.length
+          previous_word = result_array[index-1]
+
+           unless previous_word[i] == word[i]
+            final_array << word
+            i = word.length
+          else
+            #compare the two strings
+            final_array.pop
+            final_array << compare_strings(previous_word, word)
+
+
+           end
+
+           i += 1
+
+         end
+      else
+        final_array << word
+      end
+  end
+
+ end
